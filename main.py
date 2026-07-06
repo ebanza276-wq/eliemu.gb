5db2f394e5368dd6bc43226290bae6003a8af60a
ebanza276-wq <ebanza276@gmail.com>
2026-07-06 22:46:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import flet as ft
+import flet_camera as fc
 
 def main(page: ft.Page):
-    page.title = "Mon application Flet"
-
-    compteur = ft.Text(value="0", size=30)
-
-    def incrementer(e):
-        compteur.value = str(int(compteur.value) + 1)
-        page.update()
-
-    bouton = ft.ElevatedButton(
-        "Cliquez ici",
-        on_click=incrementer
-    )
+    camera = fc.Camera()
 
     page.add(
-        ft.Text("Exemple Flet"),
-        compteur,
-        bouton
+        camera
     )
 
-ft.app(target=main)
+ft.app(main)
